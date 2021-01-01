30288bceeacb6536c3298cc3331c9ea23e6b18db
Ofey Chan <ofey206@gmail.com>
2021-01-01 22:42:05 +0800

    Stage state before I change the request logic

--- yaftp-ui/app.py
+++ yaftp-ui/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 import sqlite3
 from werkzeug.exceptions import abort
 import yaftp
+import os
+from time import sleep
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -10,13 +12,29 @@ def get_db_connection():
 
 def get_yaftp_connection(yaftp_id):
     conn = get_db_connection()
-    print(yaftp_id)
     yaftp_connection = conn.execute('SELECT * FROM auth WHERE id = ?', (yaftp_id,)).fetchone()
     conn.close()
     if yaftp_connection is None:
         abort(404)
     return yaftp_connection
-    
+
+def all_filenames(datadir: str):
+    f = []
+    for (_, _, filenames) in os.walk(datadir):
+        f.extend(filenames)
+        break
+    return f
+
+def get_folders_and_files(dir_result):
+    folders = []
+    files = []
+    for entry in dir_result:
+        if entry.endswith('/'):
+            folders.append(entry)
+        else:
+            files.append(entry)
+    return (folders, files)
+
 app = Flask(__name__)
 # ALERT: Modify this.
 app.config['SECRET_KEY'] = 'fair is foul, and foul is fair'
@@ -28,18 +46,56 @@ def index():
     conn.close()
     return render_template('index.html', auths = auths)
 
+connections = {}
+
 @app.route('/<int:yaftp_id>', methods=('GET', 'POST'))
 def yaftp_connection(yaftp_id):
-    if request.method == 'POST':
-        flash('POSTED!')
     yaftp_conn = get_yaftp_connection(yaftp_id)
-    return render_template(
-        'yaftp.html', 
-        yaftp_conn=yaftp_conn,
-        files=['file1', 'dir1/'],
-        local_files=['local1', 'local2']
+
+    username = yaftp_conn['username']
+    passwd = yaftp_conn['passwd']
+    datadir = yaftp_conn['datadir']
+    host = yaftp_conn['host']
+    port = yaftp_conn['port']
+    local_files = all_filenames(datadir)
+
+    if username not in connections:
+        connections[username] = yaftp.YAFTP(
+            address=(host, int(port)),
+            user=username,
+            passwd=passwd
         )
 
+    connections[username].login()
+
+    if request.method == 'POST':
+        if "dir" in request.form:
+            dir_name = request.form["dir"]
+            
+            connections[username].cd(dir_name)
+            flash(f'cd to {dir_name}!')
+        if "file" in request.form:
+            file_name = request.form["file"]
+            savepath=os.path.join(datadir, file_name)
+            connections[username].get(name=file_name, savepath=savepath)
+            flash(f'saved {file_name} in {savepath}')
+        if "local_file" in request.form:
+            local_file_name = request.form["local_file"]
+            filepath = os.path.join(datadir, local_file_name)
+            connections[username].send(filepath=filepath, name=local_file_name)
+            path = os.path.join(connections[username].pwd(), local_file_name)
+            flash(f'sent {local_file_name} to {path}')
+
+    folders, files = get_folders_and_files(connections[username].dir())
+
+    t = render_template(
+        'yaftp.html', 
+        yaftp_conn=yaftp_conn,
+        files=files,
+        folders=folders,
+        local_files=local_files
+        )
+    return t
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
